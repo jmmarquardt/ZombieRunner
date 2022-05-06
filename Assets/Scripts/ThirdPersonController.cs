fe7791dd9071817cafc30de7f8e5e882cc85c511
John-Mike Marquardt <jmmarquardt@gmail.com>
2022-05-05 21:48:04 -0400

    CHORE: Initial Farm Layout, ThirdPersonAction Input system setup
    Setup new input system for third person character
    setup basic sandbox farm level layout for prototyping

---
+++ Assets/Scripts/ThirdPersonController.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class ThirdPersonController : MonoBehaviour
+{
+    [SerializeField] InputActionAsset _playerActionAsset;
+    [SerializeField] InputAction move;
+}
