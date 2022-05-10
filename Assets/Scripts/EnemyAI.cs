001bef585019c79ed9cbc43cc75bb8bb5d30270a
John-Mike Marquardt <jmmarquardt@gmail.com>
2022-05-10 14:32:39 -0400

    CHORE: enemy AI refactoring.

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -3,18 +3,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.Timeline;
 
 public class EnemyAI : MonoBehaviour
 {
     [SerializeField] 
     Transform _target;
     
-    [SerializeField] [Range(0f, 10f)] 
-    float _chaseRange = 5f;
+    [SerializeField] [Range(0f, 20f)] 
+    float _chaseRange = 10f;
 
     float _distanceToTarget = Mathf.Infinity;
     
     NavMeshAgent _navMeshAgent;
+    bool _isProvoked;
 
     void Awake()
     {
@@ -23,15 +25,53 @@ public class EnemyAI : MonoBehaviour
 
     void Update()
     {
-        FollowPlayer();
+        CheckTarget();
     }
 
-    void FollowPlayer()
+    void CheckTarget()
     {
-        var targetPos = _target.position;
-        _distanceToTarget = Vector3.Distance(transform.position, targetPos);
-        if (_distanceToTarget > _chaseRange) { return; }
+        _distanceToTarget = Vector3.Distance(transform.position, _target.position);
+        if (_isProvoked)
+        {
+            EngageTarget(_target.position);
+        }
+        else if (_distanceToTarget <= _chaseRange)
+        {
+            _isProvoked = true;
+        }
+        else
+        {
+            _isProvoked = false;
+        }
+    }
+
+    void EngageTarget(Vector3 targetPos)
+    {
+        if (_distanceToTarget >= _navMeshAgent.stoppingDistance)
+        {
+            ChaseTarget();
+        }
+
+        if (_distanceToTarget < _navMeshAgent.stoppingDistance)
+        {
+            AttackTarget();
+        }
         
-        _navMeshAgent.SetDestination(targetPos);
+    }
+
+    void ChaseTarget()
+    {
+        _navMeshAgent.SetDestination(_target.position);
+    }
+    
+    void AttackTarget()
+    {
+        Debug.Log($"{name} is attacking {_target.name}!!!");
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, _chaseRange);
     }
 }
